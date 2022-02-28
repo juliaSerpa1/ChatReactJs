0a79bf5bd43aaec07c2c713836b069c8e8933e13
juliaSerpa1 <julia_jmsn@hotmail.com>
2022-02-28 16:49:34 -0400

    adicionando components

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookie from 'universal-cookie';
+
+import { ChannelContainer, ChannelListContainer } from './components'
+
+const apiKey = 'td5cm72d2jnj';
+
+const client = StreamChat.getInstance(apiKey);
+
+const App = () => {
+  return (
+    <div className='app__wrapper'>
+        <Chat client={client} theme = 'team light'>
+            <ChannelListContainer
+
+            />
+            <ChannelContainer
+
+            />
+        </Chat>
+    </div>
+  );
+}
+export default App;
